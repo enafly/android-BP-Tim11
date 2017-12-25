8fdf4319948e7f5c6e1a06435eb4531be90f0a88
enafly <emuratspah1@etf.unsa.ba>
2017-12-25 13:12:32 +0100

    Navigation menu

--- app/src/main/java/ba/unsa/etf/rma/ena/dms_android/LoginActivity.java
+++ app/src/main/java/ba/unsa/etf/rma/ena/dms_android/LoginActivity.java
@@ -3,6 +3,7 @@ package ba.unsa.etf.rma.ena.dms_android;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -100,7 +101,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         if (!mayRequestContacts()) {
             return;
         }
-
         getLoaderManager().initLoader(0, null, this);
     }
 
@@ -334,7 +334,9 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
 
             if (success) {
-                finish();
+                //finish();
+                Intent loginSuccess = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(loginSuccess);
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
                 mPasswordView.requestFocus();
